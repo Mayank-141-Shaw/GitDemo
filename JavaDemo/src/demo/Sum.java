06046b70fcd9db80dde37e2392ea2b30293afdb7
Mayank-141-Shaw <mayank141shaw@gmail.com>
2021-09-16 13:10:17 +0530

    added Sum.java

---
+++ JavaDemo/src/demo/Sum.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float s = 10*10;
+		int j = (int)(10 + s);
+		System.out.println("Total is = "+j);
+	}
+
+}
